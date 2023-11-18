3157202ac6c59522f315f660a7a2edcdc0c06ec3
Impossible380 <impossible380@tutanota.com>
2023-11-18 11:02:27 +0100

    ajout des templates

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import json
+from utils import *
 
 
 if __name__ == "__main__":
-    input_file = input("Dans quelle fichier se trouvent les données demandées ? ")
-    actual_file = open(input_file)
-    print(type(json.load(actual_file)))
+    # input_file = input("Dans quelle fichier se trouvent les données demandées ? ")
+    actual_file = open("ste-foy.json")
+    object_list = json.load(actual_file)
+    print(len(object_list))
+
+    road_list = extract_ways(object_list)
+    print(road_list, len(road_list))
--- utils.py
+++ utils.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    input("Dans quelle fichier se trouvent les données demandées")
+def extract_ways(object_list):
+    road_list = []
+    for object in object_list:
+        if object["type"] == "way":
+            road_list.append(object)
+
+    return road_list
+
+
+def create_graph(road_list):
+    for actual_road in road_list:
+        for crossing_road in road_list:
+            if actual_road["nodes"]["lat"] == crossing_road["nodes"]["lat"] and \
+                actual_road["nodes"]["lon"] == crossing_road["nodes"]["lon"]:
+                pass
+
+
+def output_json(road_list):
+    print(road_list)

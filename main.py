1d84df186f3536e28dd8aaa697083facfa55f3d6
Impossible380 <impossible380@tutanota.com>
2023-11-18 11:39:13 +0100

    Creation de la fonction output_template

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     # input_file = input("Dans quelle fichier se trouvent les données demandées ? ")
     actual_file = open("ste-foy.json")
     object_list = json.load(actual_file)
-    print(len(object_list))
 
     road_list = extract_ways(object_list)
-    print(road_list, len(road_list))
+    output_json(road_list)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 def extract_ways(object_list):
     road_list = []
     for object in object_list:
@@ -16,4 +18,31 @@ def create_graph(road_list):
 
 
 def output_json(road_list):
-    print(road_list)
+
+    # json.load, charge un Fichier json depuis le disque dans un dico / liste
+
+    # Objet standard associé à la carte
+    output_template = json.load(open("output_template.json"))
+
+    # Objet standard associé à une route
+    feature_template = json.load(open("feature_template.json"))
+
+    output_template["features"].append(feature_template)
+
+    # On itere sur chacune des routes...
+    for road in road_list:
+        feature = feature_template.copy()
+        # ...Et sur chacune des coordonnées
+        for node in road["nodes"]:
+            lat = node["lat"]
+            lon = node["lon"]
+            coordinate = [lon,lat]
+            feature["geometry"]["coordinates"].append(coordinate)
+        output_template["features"].append(feature)
+
+
+    # Bonne pratique : with open permet d'ouvrir un fichier sous un autre nom et le ferme automatiquement à la fin du bloc with
+    with open("out.json","w") as f:
+
+        # json.dumps, va traduire notre dico / liste vers une chaine de caractères (que l'on écrit sur le disque pour avoir un Fichier json :) )
+        f.write(json.dumps(output_template))
